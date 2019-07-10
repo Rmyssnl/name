984d8214bbf28263809b12fa635e9fb94201a859
Rmyssnl <asfer2015@outlook.com>
2019-07-10 17:03:35 +0300

    proje tamam

--- app/src/main/java/com/example/name/Main2Activity.java
+++ app/src/main/java/com/example/name/Main2Activity.java
@@ -3,6 +3,9 @@ package com.example.name;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        String adSoyad = getIntent().getStringExtra("Name");
+        TextView textViewName = findViewById(R.id.textView);
+        textViewName.setText("Hoşgeldin " + adSoyad);
     }
 }
--- app/src/main/java/com/example/name/MainActivity.java
+++ app/src/main/java/com/example/name/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.name;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView14;
+    EditText editText4;
+    EditText editText5;
+    EditText editText6;
+    EditText editText7;
+    Button button2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView14= findViewById(R.id.imageView14);
+        editText4= findViewById(R.id.editText4);
+        editText5= findViewById(R.id.editText5);
+        editText6= findViewById(R.id.editText6);
+        editText7= findViewById(R.id.editText7);
+        button2= findViewById(R.id.button2);
+
+    }
+
+
+    public void changeactivity(View view) {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra("Name", editText4.getText().toString());
+        startActivity(intent);
     }
 }
